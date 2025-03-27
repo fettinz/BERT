b3e3f05d131edb8080e86bc5045e81fee03c8174
fettinz <alessandro.ungarelli@students.uniroma2.eu>
2025-03-27 19:47:22 +0100

    update

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import predict as p
 import fit as f
 
 if __name__ == "__main__":
-    print("Premi 1 per addestrare il modello")
-    print("Premi 2 per testare il modello sul dataset test")
-    print("Premi 3 per inserire una frase da testare")
-    choice = input()
-    if choice == "1":
-        f.train_model()
-    elif choice == "2":
-        p.load_model()
-        p.test_model_on_test_set()
-    elif choice == "3":
-        p.load_model()
-        print("Inserisci la frase da testare")
-        sentence = input()
-        p.test_sentence(sentence)
+    while True:
+        print("-------------------------------------------------")
+        print("🤖BERT Sentiment Analysis🤖")
+        print("Premi 1 per addestrare il modello")
+        print("Premi 2 per testare il modello sul dataset test")
+        print("Premi 3 per inserire una frase da testare")
+        print("Premi 4 per uscire")
+        print("-------------------------------------------------")
+        choice = input()
+        if choice == "1":
+            f.train_model()
+        elif choice == "2":
+            p.load_model()
+            p.test_model_on_test_set()
+        elif choice == "3":
+            p.load_model()
+            print("Inserisci la frase da testare")
+            sentence = input()
+            p.test_sentence(sentence)
+        elif choice == "4":
+            break
+        else:
+            print("Scelta non valida")
+            continue
